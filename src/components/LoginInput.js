9ab23a8c63123b32529e6d67d38f1e6f20b2dfe2
Yohanes Harke Wauran <harkewauran23@gmail.com>
2022-10-26 00:04:21 +0800

    change color placeholder

--- src/components/LoginInput.js
+++ src/components/LoginInput.js
@@ -48,8 +48,8 @@ class LoginInput extends React.Component {
           ({ locale }) => {
             return (
               <form onSubmit={this.onSubmitHandler} className='flex flex-col space-y-4'>
-                <input type='email' placeholder='Email' value={this.state.email} onChange={this.onEmailChangeHandler} className='w-[80%] mx-auto bg-white text-black border-2 border-black placeholder:text-white px-4 py-2'/>
-                <input type='password' placeholder={locale === 'id' ? 'Kata Sandi' : 'Password'} value={this.state.password} onChange={this.onPasswordChangeHandler} className='w-[80%] mx-auto bg-white text-black border-2 border-black placeholder:text-white px-4 py-2' />
+                <input type='email' placeholder='Email' value={this.state.email} onChange={this.onEmailChangeHandler} className='w-[80%] mx-auto  text-black border-2 border-black placeholder:text-black px-4 py-2'/>
+                <input type='password' placeholder={locale === 'id' ? 'Kata Sandi' : 'Password'} value={this.state.password} onChange={this.onPasswordChangeHandler} className='w-[80%] mx-auto bg-white text-black border-2 border-black placeholder:text-black px-4 py-2' />
                 <button className='w-[80%] mx-auto py-2 bg-[#169E16] font-bold text-white'>{ locale === 'id' ? 'Masuk' : 'Login'}</button>
               </form>
             )
--- src/components/RegisterInput.js
+++ src/components/RegisterInput.js
@@ -61,7 +61,7 @@ class RegisterInput extends React.Component {
               <form onSubmit={this.onSubmitHandler} className='flex flex-col space-y-4'>
                 <input type='text' placeholder={ locale === 'id' ? 'Name' : 'Name'} value={this.state.name} onChange={this.onNameChange} className='w-[80%] mx-auto bg-white text-black border-2 border-black placeholder:text-black px-4 py-2' />
                 <input type='email' placeholder='Email' value={this.state.email} onChange={this.onEmailChange} className='w-[80%] mx-auto bg-white text-black border-2 border-black placeholder:text-black px-4 py-2'/>
-                <input type='password' placeholder={ locale === 'id' ? 'Kata Sandi' : 'Password'} autoComplete='current-password' value={this.state.password} onChange={this.onPasswordChange} className='w-[80%] mx-auto bg-white text-white border-2 border-black placeholder:text-white px-4 py-2'/>
+                <input type='password' placeholder={ locale === 'id' ? 'Kata Sandi' : 'Password'} autoComplete='current-password' value={this.state.password} onChange={this.onPasswordChange} className='w-[80%] mx-auto bg-white text-white border-2 border-black placeholder:text-black px-4 py-2'/>
                 <button className='w-[80%] mx-auto py-2 bg-[#169E16] font-bold text-white'>{locale === 'id' ? 'Daftar' : 'Register'}</button>
               </form>
             )
